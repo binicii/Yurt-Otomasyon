3a08357c51f84e4b9f62c1e20e4edda0e85b0754
Büşra BİNİCİ <36550410+binicii@users.noreply.github.com>
2021-06-22 00:34:28 +0300

    FrmAblok oluşturuldu.

---
+++ YurtOtomasyonu/FrmAblok.Designer.cs
@@ -0,0 +1,51 @@
+﻿
+namespace YurtOtomasyonu
+{
+    partial class FrmAblok
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAblok));
+            this.SuspendLayout();
+            // 
+            // FrmAblok
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1027, 493);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmAblok";
+            this.Text = "A BLOK STOK TAKİBİ";
+            this.Load += new System.EventHandler(this.FrmAblok_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ YurtOtomasyonu/FrmAblok.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YurtOtomasyonu
+{
+    public partial class FrmAblok: Form
+    {
+        public FrmAblok()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmAblok_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'yurtOtomasyonuDataSet15.A_Blok_Stok' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.a_Blok_StokTableAdapter.Fill(this.yurtOtomasyonuDataSet15.A_Blok_Stok);
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
