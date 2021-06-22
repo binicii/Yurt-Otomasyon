ffc4be6ba1b2f51d4d6354c12959b55018ef5d53
zahidkaraca <36535478+zahidkaraca@users.noreply.github.com>
2021-06-23 00:38:05 +0300

    B Blok stok silme
    Stok silme işlemi yapılmıştır.

---
+++ YurtOtomasyonu/B_blok_stok_silme.cs
@@ -0,0 +1,9 @@
+private void Sil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komutsil = new SqlCommand("delete from B_Blok_Stok where Bblok_id=@k6", baglanti);
+            komutsil.Parameters.AddWithValue("@k6", TxtBblokid.Text);
+            komutsil.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kayıt Silindi");
+        }
