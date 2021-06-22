72d3539117155bcc9635a4741b93be2ab3f08f32
zahidkaraca <36535478+zahidkaraca@users.noreply.github.com>
2021-06-23 00:00:54 +0300

    B Blok Stok işlemi servis bağlamı
    Servisler bağlanmıştır.

---
+++ YurtOtomasyonu/B_blok_servis_baglama.cs
@@ -0,0 +1,21 @@
+using DocumentFormat.OpenXml.Office2010.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YurtOtomasyonu
+{
+    public partial class FrmBblok : Form
+    {
+        public FrmBblok()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-HMA2FOJ;Initial Catalog=YurtOtomasyonu;Integrated Security=True");
