6df18b8503177b8bb9fa7e54ef815598d15953e5
zahidkaraca <zahidkaraca55@gmail.com>
2021-06-23 13:07:07 +0300

    Hatalar Düzeltildi 1

--- YurtOtomasyonu/FrmAblok.Designer.cs
+++ YurtOtomasyonu/FrmAblok.Designer.cs
@@ -35,7 +35,7 @@ namespace YurtOtomasyonu
             this.yurtOtomasyonuDataSet15 = new YurtOtomasyonu.YurtOtomasyonuDataSet15();
             this.aBlokStokBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Guncelle = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.TxtDolapSayisi = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
@@ -56,7 +56,7 @@ namespace YurtOtomasyonu
             this.masaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.yatakDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Kaydet = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.yurtOtomasyonuDataSet15)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.aBlokStokBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -87,15 +87,16 @@ namespace YurtOtomasyonu
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button2
+            // Guncelle
             // 
-            this.button2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button2.Location = new System.Drawing.Point(279, 416);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(136, 40);
-            this.button2.TabIndex = 14;
-            this.button2.Text = "GÜNCELLE";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Guncelle.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Guncelle.Location = new System.Drawing.Point(279, 416);
+            this.Guncelle.Name = "Guncelle";
+            this.Guncelle.Size = new System.Drawing.Size(136, 40);
+            this.Guncelle.TabIndex = 14;
+            this.Guncelle.Text = "GÜNCELLE";
+            this.Guncelle.UseVisualStyleBackColor = true;
+            this.Guncelle.Click += new System.EventHandler(this.Guncelle_Click);
             // 
             // label7
             // 
@@ -284,16 +285,18 @@ namespace YurtOtomasyonu
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(513, 336);
             this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
-            // button1
+            // Kaydet
             // 
-            this.button1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(16, 416);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 40);
-            this.button1.TabIndex = 17;
-            this.button1.Text = "KAYDET";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Kaydet.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Kaydet.Location = new System.Drawing.Point(16, 416);
+            this.Kaydet.Name = "Kaydet";
+            this.Kaydet.Size = new System.Drawing.Size(97, 40);
+            this.Kaydet.TabIndex = 17;
+            this.Kaydet.Text = "KAYDET";
+            this.Kaydet.UseVisualStyleBackColor = true;
+            this.Kaydet.Click += new System.EventHandler(this.Kaydet_Click);
             // 
             // FrmAblok
             // 
@@ -301,10 +304,10 @@ namespace YurtOtomasyonu
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1027, 493);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Kaydet);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Guncelle);
             this.Controls.Add(this.TxtDolapSayisi);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.TxtMasaSayisi);
@@ -336,7 +339,7 @@ namespace YurtOtomasyonu
         private YurtOtomasyonuDataSet15 yurtOtomasyonuDataSet15;
         private System.Windows.Forms.BindingSource aBlokStokBindingSource;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Guncelle;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox TxtDolapSayisi;
         private System.Windows.Forms.Label label6;
@@ -357,6 +360,6 @@ namespace YurtOtomasyonu
         private System.Windows.Forms.DataGridViewTextBoxColumn masaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn yatakDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Kaydet;
     }
 }
--- YurtOtomasyonu/FrmAblok.cs
+++ YurtOtomasyonu/FrmAblok.cs
@@ -56,6 +56,60 @@ namespace YurtOtomasyonu
             }
         }
 
+
+        private void Kaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                baglanti.Open();
+                SqlCommand komutkaydet = new SqlCommand("insert into A_Blok_Stok (yatak,masa,sandalye,dolap,komodin,Ablok_id) values(@k1,@k2,@k3,@k4,@k5,k6)", baglanti);
+                komutkaydet.Parameters.AddWithValue("@k1", TxtYatakSayisi.Text);
+                komutkaydet.Parameters.AddWithValue("@k2", TxtMasaSayisi.Text);
+                komutkaydet.Parameters.AddWithValue("@k3", TxtSandalyeSayisi.Text);
+                komutkaydet.Parameters.AddWithValue("@k4", TxtDolapSayisi.Text);
+                komutkaydet.Parameters.AddWithValue("@k5", TxtKomodinSayisi.Text);
+                komutkaydet.Parameters.AddWithValue("@k6", TxtAblokid.Text);
+
+                komutkaydet.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Kayıt Başarılı Bir Şekilde Gerçekleşmiştir");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Hata!!! Tekrar Deneyiniz");
+            }
+        }
+
+        private void Guncelle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("update A_Blok_Stok set yatak=@k1,masa=@k2,sandalye=@k3,dolap=@k4,komodin=@k5 where Ablok_id=@k6", baglanti);
+                komut.Parameters.AddWithValue("@k1", TxtYatakSayisi.Text);
+                komut.Parameters.AddWithValue("@k2", TxtMasaSayisi.Text);
+                komut.Parameters.AddWithValue("@k3", TxtSandalyeSayisi.Text);
+                komut.Parameters.AddWithValue("@k4", TxtDolapSayisi.Text);
+                komut.Parameters.AddWithValue("@k5", TxtKomodinSayisi.Text);
+                komut.Parameters.AddWithValue("@k6", TxtAblokid.Text);
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Kayıt Başarıyla Güncellendi");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("HATA!!!!! ");
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
     }
+
 }
 
