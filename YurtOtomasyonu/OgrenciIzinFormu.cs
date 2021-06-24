82cf3536bc2e1b56908ebd26d2fc97c6a874a83a
Büşra BİNİCİ <36550410+binicii@users.noreply.github.com>
2021-06-24 21:32:39 +0300

    Öğrenci İzin Formu Oluşturuldu.

---
+++ YurtOtomasyonu/OgrenciIzinFormu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YurtOtomasyonu
+{
+    public partial class OgrenciIzinFormu : Form
+    {
+        public OgrenciIzinFormu()
+        {
+            InitializeComponent();
+        }
+
+        private void OgrenciIzinFormu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
