129cc54321c66ca4dce5904f02749d23328051fb
zahidkaraca <zahidkaraca55@gmail.com>
2021-06-25 17:29:01 +0300

    Misafir Öğrenci Sayfası Tasarımı Yapıldı

--- YurtOtomasyonu/MisafirOgrenciFormu.Designer.cs
+++ YurtOtomasyonu/MisafirOgrenciFormu.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace YurtOtomasyonu
+﻿namespace YurtOtomasyonu
 {
     partial class MisafirOgrenciFormu
     {
@@ -29,12 +28,324 @@ namespace YurtOtomasyonu
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CmbOgrodaNo = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtOgrMail = new System.Windows.Forms.TextBox();
+            this.TxtMail = new System.Windows.Forms.Label();
+            this.CmbOgrBolum = new System.Windows.Forms.ComboBox();
+            this.TxtOgrBolum = new System.Windows.Forms.Label();
+            this.MsbOgrDogumt = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MskOgrTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.TxtOgrTelefon = new System.Windows.Forms.Label();
+            this.MsbOgrTC = new System.Windows.Forms.MaskedTextBox();
+            this.TxtOgrTC = new System.Windows.Forms.Label();
+            this.TxtOgrSoyad = new System.Windows.Forms.TextBox();
+            this.TxtSoyad = new System.Windows.Forms.Label();
+            this.TxtOgrAd = new System.Windows.Forms.TextBox();
+            this.TxtAd = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MsbGirisTarihi = new System.Windows.Forms.MaskedTextBox();
+            this.MsbCikisT = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmbOgrodaNo
+            // 
+            this.CmbOgrodaNo.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.CmbOgrodaNo.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CmbOgrodaNo.FormattingEnabled = true;
+            this.CmbOgrodaNo.Location = new System.Drawing.Point(224, 396);
+            this.CmbOgrodaNo.Name = "CmbOgrodaNo";
+            this.CmbOgrodaNo.Size = new System.Drawing.Size(246, 32);
+            this.CmbOgrodaNo.TabIndex = 31;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(117, 401);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 23);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Oda No:";
+            // 
+            // TxtOgrMail
+            // 
+            this.TxtOgrMail.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.TxtOgrMail.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtOgrMail.Location = new System.Drawing.Point(224, 352);
+            this.TxtOgrMail.Name = "TxtOgrMail";
+            this.TxtOgrMail.Size = new System.Drawing.Size(246, 30);
+            this.TxtOgrMail.TabIndex = 29;
+            // 
+            // TxtMail
+            // 
+            this.TxtMail.AutoSize = true;
+            this.TxtMail.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtMail.Location = new System.Drawing.Point(143, 359);
+            this.TxtMail.Name = "TxtMail";
+            this.TxtMail.Size = new System.Drawing.Size(55, 23);
+            this.TxtMail.TabIndex = 28;
+            this.TxtMail.Text = "Mail:";
+            // 
+            // CmbOgrBolum
+            // 
+            this.CmbOgrBolum.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.CmbOgrBolum.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CmbOgrBolum.FormattingEnabled = true;
+            this.CmbOgrBolum.Location = new System.Drawing.Point(224, 301);
+            this.CmbOgrBolum.Name = "CmbOgrBolum";
+            this.CmbOgrBolum.Size = new System.Drawing.Size(246, 32);
+            this.CmbOgrBolum.TabIndex = 27;
+            // 
+            // TxtOgrBolum
+            // 
+            this.TxtOgrBolum.AutoSize = true;
+            this.TxtOgrBolum.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtOgrBolum.Location = new System.Drawing.Point(55, 302);
+            this.TxtOgrBolum.Name = "TxtOgrBolum";
+            this.TxtOgrBolum.Size = new System.Drawing.Size(143, 23);
+            this.TxtOgrBolum.TabIndex = 26;
+            this.TxtOgrBolum.Text = "Öğrenci Bölüm:";
+            // 
+            // MsbOgrDogumt
+            // 
+            this.MsbOgrDogumt.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.MsbOgrDogumt.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MsbOgrDogumt.Location = new System.Drawing.Point(224, 247);
+            this.MsbOgrDogumt.Mask = "00/00/0000";
+            this.MsbOgrDogumt.Name = "MsbOgrDogumt";
+            this.MsbOgrDogumt.Size = new System.Drawing.Size(246, 30);
+            this.MsbOgrDogumt.TabIndex = 25;
+            this.MsbOgrDogumt.ValidatingType = typeof(System.DateTime);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(68, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 23);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Doğum Tarihi";
+            // 
+            // MskOgrTelefon
+            // 
+            this.MskOgrTelefon.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.MskOgrTelefon.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskOgrTelefon.Location = new System.Drawing.Point(224, 192);
+            this.MskOgrTelefon.Mask = "(999) 000-0000";
+            this.MskOgrTelefon.Name = "MskOgrTelefon";
+            this.MskOgrTelefon.Size = new System.Drawing.Size(246, 30);
+            this.MskOgrTelefon.TabIndex = 23;
+            // 
+            // TxtOgrTelefon
+            // 
+            this.TxtOgrTelefon.AutoSize = true;
+            this.TxtOgrTelefon.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtOgrTelefon.Location = new System.Drawing.Point(120, 192);
+            this.TxtOgrTelefon.Name = "TxtOgrTelefon";
+            this.TxtOgrTelefon.Size = new System.Drawing.Size(78, 23);
+            this.TxtOgrTelefon.TabIndex = 22;
+            this.TxtOgrTelefon.Text = "Telefon:";
+            // 
+            // MsbOgrTC
+            // 
+            this.MsbOgrTC.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.MsbOgrTC.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MsbOgrTC.Location = new System.Drawing.Point(224, 146);
+            this.MsbOgrTC.Mask = "00000000000";
+            this.MsbOgrTC.Name = "MsbOgrTC";
+            this.MsbOgrTC.Size = new System.Drawing.Size(246, 30);
+            this.MsbOgrTC.TabIndex = 21;
+            this.MsbOgrTC.ValidatingType = typeof(int);
+            // 
+            // TxtOgrTC
+            // 
+            this.TxtOgrTC.AutoSize = true;
+            this.TxtOgrTC.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtOgrTC.Location = new System.Drawing.Point(158, 145);
+            this.TxtOgrTC.Name = "TxtOgrTC";
+            this.TxtOgrTC.Size = new System.Drawing.Size(40, 23);
+            this.TxtOgrTC.TabIndex = 20;
+            this.TxtOgrTC.Text = "TC:";
+            // 
+            // TxtOgrSoyad
+            // 
+            this.TxtOgrSoyad.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.TxtOgrSoyad.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtOgrSoyad.Location = new System.Drawing.Point(224, 104);
+            this.TxtOgrSoyad.Name = "TxtOgrSoyad";
+            this.TxtOgrSoyad.Size = new System.Drawing.Size(246, 30);
+            this.TxtOgrSoyad.TabIndex = 19;
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.AutoSize = true;
+            this.TxtSoyad.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtSoyad.Location = new System.Drawing.Point(59, 104);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(139, 23);
+            this.TxtSoyad.TabIndex = 18;
+            this.TxtSoyad.Text = "Öğrenci Soyad:";
+            // 
+            // TxtOgrAd
+            // 
+            this.TxtOgrAd.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.TxtOgrAd.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtOgrAd.Location = new System.Drawing.Point(224, 56);
+            this.TxtOgrAd.Name = "TxtOgrAd";
+            this.TxtOgrAd.Size = new System.Drawing.Size(246, 30);
+            this.TxtOgrAd.TabIndex = 17;
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.AutoSize = true;
+            this.TxtAd.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtAd.Location = new System.Drawing.Point(86, 56);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(112, 23);
+            this.TxtAd.TabIndex = 16;
+            this.TxtAd.Text = "Öğrenci Ad:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(107, 616);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 50);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(265, 616);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 50);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "Güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(423, 616);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 50);
+            this.button3.TabIndex = 34;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(82, 459);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 23);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "Giriş Tarihi:";
+            // 
+            // MsbGirisTarihi
+            // 
+            this.MsbGirisTarihi.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MsbGirisTarihi.Location = new System.Drawing.Point(224, 459);
+            this.MsbGirisTarihi.Mask = "00/00/0000";
+            this.MsbGirisTarihi.Name = "MsbGirisTarihi";
+            this.MsbGirisTarihi.Size = new System.Drawing.Size(246, 28);
+            this.MsbGirisTarihi.TabIndex = 37;
+            this.MsbGirisTarihi.ValidatingType = typeof(System.DateTime);
+            // 
+            // MsbCikisT
+            // 
+            this.MsbCikisT.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MsbCikisT.Location = new System.Drawing.Point(224, 516);
+            this.MsbCikisT.Mask = "00/00/0000";
+            this.MsbCikisT.Name = "MsbCikisT";
+            this.MsbCikisT.Size = new System.Drawing.Size(246, 28);
+            this.MsbCikisT.TabIndex = 39;
+            this.MsbCikisT.ValidatingType = typeof(System.DateTime);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(82, 516);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 23);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Çıkış Tarihi:";
+            // 
+            // MisafirOgrenciFormu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MisafirOgrenciFormu";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(932, 699);
+            this.Controls.Add(this.MsbCikisT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MsbGirisTarihi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CmbOgrodaNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtOgrMail);
+            this.Controls.Add(this.TxtMail);
+            this.Controls.Add(this.CmbOgrBolum);
+            this.Controls.Add(this.TxtOgrBolum);
+            this.Controls.Add(this.MsbOgrDogumt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MskOgrTelefon);
+            this.Controls.Add(this.TxtOgrTelefon);
+            this.Controls.Add(this.MsbOgrTC);
+            this.Controls.Add(this.TxtOgrTC);
+            this.Controls.Add(this.TxtOgrSoyad);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.TxtOgrAd);
+            this.Controls.Add(this.TxtAd);
+            this.Name = "MisafirOgrenciFormu";
+            this.Text = "misafirOgr";
+            this.Load += new System.EventHandler(this.MisafirOgrenciFormu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox CmbOgrodaNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtOgrMail;
+        private System.Windows.Forms.Label TxtMail;
+        private System.Windows.Forms.ComboBox CmbOgrBolum;
+        private System.Windows.Forms.Label TxtOgrBolum;
+        private System.Windows.Forms.MaskedTextBox MsbOgrDogumt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox MskOgrTelefon;
+        private System.Windows.Forms.Label TxtOgrTelefon;
+        private System.Windows.Forms.MaskedTextBox MsbOgrTC;
+        private System.Windows.Forms.Label TxtOgrTC;
+        private System.Windows.Forms.TextBox TxtOgrSoyad;
+        private System.Windows.Forms.Label TxtSoyad;
+        private System.Windows.Forms.TextBox TxtOgrAd;
+        private System.Windows.Forms.Label TxtAd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox MsbGirisTarihi;
+        private System.Windows.Forms.MaskedTextBox MsbCikisT;
+        private System.Windows.Forms.Label label4;
     }
 }
--- YurtOtomasyonu/MisafirOgrenciFormu.cs
+++ YurtOtomasyonu/MisafirOgrenciFormu.cs
@@ -16,5 +16,28 @@ namespace YurtOtomasyonu
         {
             InitializeComponent();
         }
+
+
+        private void MisafirOgrenciFormu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
